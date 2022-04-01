d7bc0ff18c1e4bcf1681f8c09e14f63ab1b46b97
DaigoWakabayashi <mamushi.journey@gmail.com>
2022-04-01 12:55:55 +0900

    #18 TwitterAPI のセットアップ、ねこ関連ツイートの取得

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,9 +1,18 @@
 import * as functions from 'firebase-functions'
+import TwitterApi from 'twitter-api-v2'
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+// APIクライアントの作成（https://github.com/plhery/node-twitter-api-v2/blob/HEAD/doc/basics.md#client-basics）
+const client = new TwitterApi(functions.config().twitter.bearer_token)
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info('Hello logs!', { structuredData: true })
-  response.send('Hello from Firebase!')
-})
+export const searchTweets = functions.https.onRequest(
+  async (request, response) => {
+    // 直近7日間のツイートを検索する（https://github.com/plhery/node-twitter-api-v2/blob/HEAD/doc/v2.md#search-tweets-recent）
+    const catTweets = await client.v2.search('ねこ', {
+      'media.fields': 'url',
+      max_results: 100,
+    })
+    for await (const tweet of catTweets) {
+      console.log(`Tweet %j`, tweet)
+    }
+  }
+)
