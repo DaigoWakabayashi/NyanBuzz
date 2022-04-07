16b8f4f307b5aa2175015cfdbfca878318cc50e4
DaigoWakabayashi <mamushi.journey@gmail.com>
2022-04-07 21:14:50 +0900

    #36 AdminSDK で Firestore への書き込みを確認

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,7 +1,14 @@
+import * as firebaseAdmin from 'firebase-admin'
 import * as functions from 'firebase-functions'
 import TwitterApi from 'twitter-api-v2'
 
-// APIクライアントの作成（https://github.com/plhery/node-twitter-api-v2/blob/HEAD/doc/basics.md#client-basics）
+// Firebaseプロジェクトの初期化
+firebaseAdmin.initializeApp(functions.config().firebase)
+
+/// Firestore（AdminSDK）
+const db = firebaseAdmin.firestore()
+
+/// APIクライアントの作成（https://github.com/plhery/node-twitter-api-v2/blob/HEAD/doc/basics.md#client-basics）
 const client = new TwitterApi(functions.config().twitter.bearer_token)
 
 export const searchTweets = functions
@@ -11,6 +18,10 @@ export const searchTweets = functions
   .onRun(async (context) => {
     try {
       console.log('This function is running')
+      await db.collection('tweets').add({
+        text: 'にゃんにゃん',
+        createdAt: firebaseAdmin.firestore.FieldValue.serverTimestamp(),
+      })
       // // 直近7日間のツイートを検索する（https://github.com/plhery/node-twitter-api-v2/blob/HEAD/doc/v2.md#search-tweets-recent）
       // const catTweets = await client.v2.search('ねこ', {
       //   'media.fields': 'url',
