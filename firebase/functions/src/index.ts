55516f6e40f07efbfdb3125faf39ac7384634bc8
DaigoWakabayashi <mamushi.journey@gmail.com>
2022-04-08 22:40:30 +0900

    #29 「ねこ」という文字列を含んだツイートを 10件取得する

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,6 +1,6 @@
 import * as firebaseAdmin from 'firebase-admin'
 import * as functions from 'firebase-functions'
-// import TwitterApi from 'twitter-api-v2'
+import TwitterApi from 'twitter-api-v2'
 
 // Firebaseプロジェクトの初期化
 firebaseAdmin.initializeApp(functions.config().firebase)
@@ -9,7 +9,7 @@ firebaseAdmin.initializeApp(functions.config().firebase)
 const db = firebaseAdmin.firestore()
 
 /// APIクライアントの作成（https://github.com/plhery/node-twitter-api-v2/blob/HEAD/doc/basics.md#client-basics）
-// const client = new TwitterApi(functions.config().twitter.bearer_token)
+const client = new TwitterApi(functions.config().twitter.bearer_token)
 
 export const searchTweets = functions
   .region('asia-northeast1')
@@ -22,14 +22,15 @@ export const searchTweets = functions
         text: 'にゃんにゃん',
         createdAt: firebaseAdmin.firestore.FieldValue.serverTimestamp(),
       })
-      // // 直近7日間のツイートを検索する（https://github.com/plhery/node-twitter-api-v2/blob/HEAD/doc/v2.md#search-tweets-recent）
-      // const catTweets = await client.v2.search('ねこ', {
-      //   'media.fields': 'url',
-      //   max_results: 100,
-      // })
-      // for await (const tweet of catTweets) {
-      //   console.log(`Tweet %j`, tweet)
-      // }
+      // 直近7日間のツイートを検索する（https://github.com/plhery/node-twitter-api-v2/blob/HEAD/doc/v2.md#search-tweets-recent）
+      const response = await client.v2.get('tweets/search/recent', {
+        query: 'ねこ',
+        max_results: 10,
+      })
+      const catTweets = response.data
+      for (const tweet of catTweets) {
+        console.log(`ツイート : %j`, tweet)
+      }
       return null
     } catch (error) {
       console.log('エラー :', error)
