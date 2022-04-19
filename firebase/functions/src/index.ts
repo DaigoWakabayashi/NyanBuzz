0304a3f2862b470d2d37848e62adb971b836b1b1
DaigoWakabayashi <mamushi.journey@gmail.com>
2022-04-19 20:19:12 +0900

    #29 v1.1 に移行

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -9,40 +9,67 @@ firebaseAdmin.initializeApp(functions.config().firebase)
 const db = firebaseAdmin.firestore()
 
 /// APIクライアントの作成（https://github.com/plhery/node-twitter-api-v2/blob/HEAD/doc/basics.md#client-basics）
-const client = new TwitterApi(functions.config().twitter.bearer_token)
+const client = new TwitterApi({
+  appKey: functions.config().twitter.app_key,
+  appSecret: functions.config().twitter.app_secret,
+})
 
 export const searchTweets = functions
   .region('asia-northeast1')
-  .pubsub.schedule('every 5 minutes from 0:00 to 23:59')
+  .pubsub.schedule('every 15 minutes from 0:00 to 23:59')
   .timeZone('Asia/Tokyo')
   .onRun(async (context) => {
+    console.log('============ start ============')
     try {
-      // 直近7日間のツイートを検索する（https://github.com/plhery/node-twitter-api-v2/blob/HEAD/doc/v2.md#search-tweets-recent）
       // クエリ条件：
       // - 日本語
-      // - リツイートでない
-      // - 引用リツイートではない
-      // - リプライでない
+      // - 画像 or 動画付き
+      // - 最低 500 いいね
       // - (ねこ OR 猫 OR ネコ OR ﾈｺ OR にゃんこ OR ニャンコ OR ﾆｬﾝｺ) を含む
-      // - 画像つき
-      const response = await client.v2.get('tweets/search/recent', {
-        query:
-          '(ねこ OR 猫 OR ネコ OR ﾈｺ OR にゃんこ OR ニャンコ OR ﾆｬﾝｺ) -is:retweet -is:reply -is:quote has:media lang:ja',
+      const response = await client.v1.get('search/tweets.json', {
+        q: 'lang:ja filter:media min_faves:500 ねこ OR 猫 OR ネコ OR ﾈｺ OR にゃんこ OR ニャンコ OR ﾆｬﾝｺ',
         max_results: 100,
-        expansions: ['author_id'],
-        'tweet.fields': ['created_at', 'public_metrics', 'possibly_sensitive'],
-        'user.fields': ['profile_image_url'],
+        tweet_mode: 'extended',
       })
-      const catTweets = response.data
-      for (const tweet of catTweets) {
-        // ドキュメントIDをツイートIDと一致させることで、
-        // 同一のツイートを取得した場合でも update 処理となる
-        await db.collection('tweets').doc(tweet['id']).set(tweet)
-        console.log(`ツイート ${catTweets.indexOf(tweet)} %j`, tweet)
+      const batch = db.batch()
+      for (const tweet of response.statuses) {
+        if (tweet.possibly_sensitive) {
+          // センシティブなツイート
+          console.log(
+            `センシティブなTweet${response.statuses.indexOf(tweet)} : %j`,
+            tweet
+          )
+        } else {
+          // 通常のツイート
+          const tweetDoc = db.collection('tweets').doc(tweet.id_str)
+          batch.set(
+            tweetDoc,
+            {
+              id: tweet.id,
+              id_str: tweet.id_str,
+              name: tweet.user.name,
+              screen_name: tweet.user.screen_name,
+              profile_image_url_https: tweet.user.profile_image_url_https,
+              created_at: tweet.created_at,
+              favorite_count: tweet.favorite_count,
+              retweet_count: tweet.retweet_count,
+              images: tweet.extended_entities.media,
+              text: tweet.full_text.replace(
+                /https?:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+/g,
+                ''
+              ),
+            },
+            { merge: true }
+          )
+          console.log(`Tweet${response.statuses.indexOf(tweet)} : %j`, tweet)
+        }
       }
-      return null
+      await batch.commit()
+      console.log(`取得したツイート数 :`, response.statuses.length)
+      return console.log('============ end ============')
     } catch (error) {
-      console.log('エラー :', error)
-      return null
+      console.log('エラーが発生しました %j', error)
+      console.error(error)
+      return console.log('============ end ============')
     }
   })
